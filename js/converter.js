830fd247d6f37254122b6153f48b2c1e3cb1bd97
= <hdubbs77@gmail.com>
2020-10-29 13:14:29 -0400

    added rounding and edge case handling

--- js/converter.js
+++ js/converter.js
@@ -3,6 +3,7 @@
 
 let conversionRate = 0;
 
+//get initial rate
 $(document).ready(function(e){
 	$('.amountText').prop('disabled', true);
 	updateRate(function(){
@@ -11,6 +12,7 @@ $(document).ready(function(e){
 	});
 });
 
+//handle changes to currency type
 $(document).on('change', '.currencySelect', function(e){
 	$('.amountText').prop('disabled', true);
 	if(e.target.id === 'fromCurrency'){
@@ -26,32 +28,45 @@ $(document).on('change', '.currencySelect', function(e){
 	}
 });
 
+//handle changes to currency amount
 $(document).on('change', '#fromAmount', updateToAmount);
 $(document).on('change', '#toAmount', updateFromAmount);
 
 
 function updateRate(callback){
-	$.ajax({
-		url: 'https://api.exchangeratesapi.io/latest',
-		method: 'GET',
-		data: {
-			base: $('#fromCurrency').val(),
-			symbols: $('#toCurrency').val()
-		},
-		success: function(response){
-			$.each(response.rates, function(k, v){
-				conversionRate = v;
-			});
-	$('#conversionRateDisp').html('1 ' + $('#fromCurrency').val() + ' equals ' + conversionRate + ' ' + $('#toCurrency').val());
-			callback();
-		}
-	});
+	//prevent looking up a conversion rate to itself
+	if($('#fromCurrency').val() !== $('#toCurrency').val()){
+		$.ajax({
+			url: 'https://api.exchangeratesapi.io/latest',
+			method: 'GET',
+			data: {
+				base: $('#fromCurrency').val(),
+				symbols: $('#toCurrency').val()
+			},
+			success: function(response){
+				$.each(response.rates, function(k, v){
+					conversionRate = v;
+				});
+				$('#conversionRateDisp').html('1 ' + $('#fromCurrency').val() + ' equals ' + conversionRate + ' ' + $('#toCurrency').val());
+				callback();
+			}
+		});
+	}else{
+		conversionRate = 1;
+		$('#conversionRateDisp').html('1 ' + $('#fromCurrency').val() + ' equals ' + conversionRate + ' ' + $('#toCurrency').val());
+		callback();
+	}
+	
 };
 
 function updateFromAmount(){
-	$('#fromAmount').val($('#toAmount').val() / conversionRate);
+	$('#fromAmount').val(roundCurrency($('#toAmount').val() / conversionRate));
 }
 
 function updateToAmount(){
-	$('#toAmount').val($('#fromAmount').val() * conversionRate);
+	$('#toAmount').val(roundCurrency($('#fromAmount').val() * conversionRate));
+}
+
+function roundCurrency(x){
+	return Math.round(x * 100) / 100;
 }
